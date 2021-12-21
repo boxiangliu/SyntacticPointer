3d74f568f832aa8d1b1f3aeec38a2fc134350b39
Boxiang Liu <jollier.liu@gmail.com>
2021-12-20 16:37:26 -0800

    debug

--- experiments/scripts/train.sh
+++ experiments/scripts/train.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -u parsing.py --mode train --config configs/parsing/l2r.json --num_epochs 600 --batch_size 32 \
  --opt adam --learning_rate 0.001 --lr_decay 0.999997 --beta1 0.9 --beta2 0.9 --eps 1e-4 --grad_clip 5.0 \
  --loss_type token --warmup_steps 40 --reset 20 --weight_decay 0.0 --unk_replace 0.5 --beam 10 \
- --word_embedding sskip --word_path "embs/sskip.en.gz" --char_embedding random \
+ --word_embedding sskip --word_path "embs/sskip.eng.100.gz" --char_embedding random \
  --punctuation '.' '``' "''" ':' ',' \
  --train "data/ptb-train.conllx" \
  --dev "data/ptb-dev.conllx" \
