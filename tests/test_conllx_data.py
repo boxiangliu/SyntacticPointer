0679ad0805c7d36470fcf8abe056542e13be58bc
boxiangliu <jollier.liu@gmail.com>
2022-01-14 09:47:04 -0800

    added test_conllx_data.py

--- tests/test_conllx_data.py
+++ tests/test_conllx_data.py
@@ -1,5 +1,6 @@
 from lightning.io.conllx_data import create_alphabets, read_bucketed_data, read_data
 from lightning import utils
+import pytest
 
 word_embedding = "sskip"
 word_path = "experiments/embs/sskip.eng.100.gz"
@@ -10,9 +11,14 @@ dev_path = "experiments/data/ptb-dev.conllx"
 test_path = "experiments/data/ptb-test.conllx"
 
 
-def test_create_alphabets():
+@pytest.fixture
+def word_dict():
     word_dict, word_dim = utils.load_embedding_dict(word_embedding, word_path)
+    return word_dict
 
+
+@pytest.fixture
+def alphabets(word_dict):
     word_alphabet, char_alphabet, pos_alphabet, type_alphabet = create_alphabets(
         alphabet_path,
         train_path,
@@ -20,6 +26,12 @@ def test_create_alphabets():
         embedd_dict=word_dict,
         max_vocabulary_size=200000,
     )
+
+    return word_alphabet, char_alphabet, pos_alphabet, type_alphabet
+
+
+def test_create_alphabets(alphabets):
+    word_alphabet, char_alphabet, pos_alphabet, type_alphabet = alphabets
 
     # The Alphabet class contains all possible word/character/part-of-speech tage/edge types
     for word in ["_PAD", "for", "to"]:
@@ -35,5 +47,29 @@ def test_create_alphabets():
         assert type in type_alphabet.instances
 
 
-def test_read_data():
-    read_data(train_path)
+def test_read_data(alphabets):
+    word_alphabet, char_alphabet, pos_alphabet, type_alphabet = alphabets
+    word_alphabet, char_alphabet, pos_alphabet, type_alphabet = create_alphabets(
+        alphabet_path,
+        train_path,
+        data_paths=[dev_path, test_path],
+        embedd_dict=word_dict,
+        max_vocabulary_size=200000,
+    )
+
+    data_tensor, data_size = read_data(
+        train_path,
+        word_alphabet,
+        char_alphabet,
+        pos_alphabet,
+        type_alphabet,
+        max_size=None,
+        normalize_digits=True,
+        symbolic_root=False,
+        symbolic_end=False,
+    )
+    # data_tensor is a dictionary with the following keys:
+    # "WORD", "CHAR", 'POS', 'HEAD', 'TYPE', 'MASK', 'SINGLE', 'LENGTH'
+    assert data_tensor["WORD"].shape[1] == 249
+    assert data_tensor["CHAR"].shape[2] == 24
+    assert data_size == 1921
